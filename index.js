82244cc33acc7adc8b727999f2ff993e49306030
Varshil13 <sharmapriyanshu91@gmail.com>
2025-04-23 16:56:09 +0530

    index.js change

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const app = express();
 require('dotenv').config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('Hello, I am Varshil Sharma! my API is live 🚀');
+});
 
 app.get('/api', (req, res) => {
   res.json({
